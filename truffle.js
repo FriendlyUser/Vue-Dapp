6a30f68c23a9f8717f38021b456dc79668c0b056
FriendlyUser <pandabeareee@gmail.com>
2018-06-24 14:10:58 -0700

    adding basic ci file, changed README.md and highlight what I expect to accomplish

---
+++ truffle.js
@@ -0,0 +1,15 @@
+module.exports = {
+  networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      gas: 500000,
+      network_id: "*" // Match any network id
+    }/*,
+    gitlab: {
+      host: "trufflesuite_ganache-cli",
+      port: 8545,
+      network_id: "*" // Match any network id
+    } */
+  }
+};

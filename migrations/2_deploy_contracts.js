a9801f859c8d81bf993aa38392e1129d1ca73706
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-09-03 20:27:17 +0000

    Deploy FriendlyUser/Vue-Dapp to github.com/FriendlyUser/Vue-Dapp.git:gh-pages

--- migrations/2_deploy_contracts.js
+++
@@ -1,6 +0,0 @@
-var Users = artifacts.require("./Users.sol");
-var TodoList = artifacts.require("./TodoList.sol")
-module.exports = function(deployer) {
-  deployer.deploy(Users);
-  deployer.deploy(TodoList);
-};

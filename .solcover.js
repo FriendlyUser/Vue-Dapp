52829105147848980114453138b7c7d2814fbb82
FriendlyUser <studentdavidli@gmail.com>
2018-09-28 21:47:27 -0700

    adding correct package

--- .solcover.js
+++ .solcover.js
@@ -1,5 +1,5 @@
 module.exports = { 
     port: 9545,
     testrpcOptions: '-p 9545 -m "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat"',
-    copyPackages: false
+    copyNodeModules: false
 };

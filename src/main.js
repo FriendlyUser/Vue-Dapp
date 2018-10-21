54298fcfbfa4c49c7028cbb31a1ecdc3d42e43db
FriendlyUser <studentdavidli@gmail.com>
2018-10-21 12:03:44 -0700

    adding new metamask changes

--- src/main.js
+++ src/main.js
@@ -20,16 +20,31 @@ import 'vuetify/dist/vuetify.min.css'
 Vue.config.productionTip = false
 Vue.use(Vuetify)
 
-window.addEventListener('load', function () {
-  if (typeof web3 !== 'undefined') {
-    console.log('Web3 injected browser: OK.')
-    window.web3 = new Web3(window.web3.currentProvider)
-  } else {
-    console.log('Web3 injected browser: Fail. You should consider trying MetaMask.')
-    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-    window.web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'))
-  }
-
+window.addEventListener('load', async () => {
+    // Modern dapp browsers...
+    if (window.ethereum) {
+        window.web3 = new Web3(ethereum);
+        try {
+            // Request account access if needed
+            await ethereum.enable();
+            // Acccounts now exposed
+            web3.eth.sendTransaction({/* ... */});
+        } catch (error) {
+            // User denied account access...
+        }
+    }
+    // Legacy dapp browsers...
+    else if (window.web3) {
+        window.web3 = new Web3(web3.currentProvider);
+        // Acccounts always exposed
+        console.log('Web3 injected browser: Fail. You should consider trying MetaMask.')
+        // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+        window.web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'))
+    }
+    // Non-dapp browsers...
+    else {
+        console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
+    }
   /* eslint-disable no-new */
   new Vue({
     el: '#app',
